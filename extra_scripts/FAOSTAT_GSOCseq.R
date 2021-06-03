8ac3713cbcc2f5eccbfdbe61fca386b745d0e094
Isabel Luotto <72745296+Isa-Luotto@users.noreply.github.com>
2021-06-03 18:11:56 +0200

    Mollweide

--- extra_scripts/FAOSTAT_GSOCseq.R
+++ extra_scripts/FAOSTAT_GSOCseq.R
@@ -15,20 +15,23 @@ library(FAOSTAT)
 library(countrycode)
 library(raster)
 library(data.table)
+library(sp)
 
 # User defined variables
 
 ## Working directory
-wd <- "C:/Users/hp/Documents/FAO/GSOCseq"
+wd <- "C:/Users/luottoi/Documents/GSOCseq/Argentina"
 
 ## GSOCseq output maps directory 
-GSOCseq_folder <- "C:/Users/hp/Documents/FAO/GSOCseq/Greece/quality check/"
+GSOCseq_folder <- "C:/Users/luottoi/Documents/GSOCseq/Argentina/"
 
-## FAOSTAT data directory
+
+## Create a folder called FAOSTAT in your working directory
+## Define a path to the FAOSTAT folder
 data_folder <- paste0(wd,"/FAOSTAT/")
 
 ## Country of interest (specified by the 3-digit ISO code)
-ISO <- "GRC"
+ISO <- "ARG"
 
 # Carbon ton to CO2 conversion
 CO2conv <- 3.67
@@ -51,27 +54,33 @@ data <- data[, c("area_code","area", "item","y2018","element", "unit")]
 
 #Convert country codes to ISO codes
 data$ISO <- countrycode(data$area_code, origin = 'fao', destination = 'iso3c')
-data <- data[data$ISO == ISO & data$item == "Agriculture total",]
+data <- data[data$ISO == ISO & data$item == "Agriculture total" |data$ISO == ISO &  data$item == "Agricultural Soils",]
 
 #Extract CO2 equivalent
-data <-  as.data.table(data)
-data <-data[data$element %like%  "(CO2eq)",]
+
+data <-data[data$element ==  "Emissions (CO2eq)",]
 
 # Extract total agricultural emissions for country of interest
 data <- na.omit(data)
 
-#Calculate total agricultural emissions in Megatonnes
+#Calculate total agricultural emissions in Gigagrams
 totagrem <- sum(data$y2018)
 
-#Load Output Maps: Relative differences 
+#Load Output Maps: Relative Sequestration Rates (SSM-BAU)
 SSM1 <- raster(paste0(GSOCseq_folder, ISO,"_GSOCseq_RSR_SSM1_Map030.tif"))
 SSM2 <- raster(paste0(GSOCseq_folder,ISO, "_GSOCseq_RSR_SSM2_Map030.tif"))
 SSM3 <- raster(paste0(GSOCseq_folder,ISO, "_GSOCseq_RSR_SSM3_Map030.tif"))
 
+#Reproject the maps to Mollweide (this will take some time and output warning messages)
+SSM1 <-projectRaster(SSM1, crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84")
+SSM2 <-projectRaster(SSM2, crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84")
+SSM3 <-projectRaster(SSM3, crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84")
+
+
 #Sum over every pixel to get total sequestration potential 
-totssm1 <- (sum(unique(SSM1[!is.na(SSM1)]))*CO2conv)/1000
-totssm2 <- (sum(unique(SSM2[!is.na(SSM2)]))*CO2conv)/1000
-totssm3 <- (sum(unique(SSM3[!is.na(SSM3)]))*CO2conv)/1000
+totssm1 <- (sum(unique(SSM1[!is.na(SSM1)]))*CO2conv*100)/1000
+totssm2 <- (sum(unique(SSM2[!is.na(SSM2)]))*CO2conv*100)/1000
+totssm3 <- (sum(unique(SSM3[!is.na(SSM3)]))*CO2conv*100)/1000
 
 
 
