64c1d6e053d11e2271ddd61bd9dc55d6754d78ff
Isabel Luotto <72745296+Isa-Luotto@users.noreply.github.com>
2021-06-04 11:26:03 +0200

    UTM and rounded results

--- extra_scripts/FAOSTAT_GSOCseq.R
+++ extra_scripts/FAOSTAT_GSOCseq.R
@@ -14,85 +14,85 @@ rm(list = ls())
 library(FAOSTAT)
 library(countrycode)
 library(raster)
-library(data.table)
 library(sp)
+
 
 # User defined variables
 
 ## Working directory
-wd <- "C:/Users/luottoi/Documents/GSOCseq/Argentina"
+wd <- "C:/Users/hp/Documents/FAO/GSOCseq"
+#wd <- "C:/Users/luottoi/Documents/GSOCseq/Argentina"
+
 
 ## GSOCseq output maps directory 
-GSOCseq_folder <- "C:/Users/luottoi/Documents/GSOCseq/Argentina/"
+GSOCseq_folder <- paste0(wd,"/Argentina/")
 
 
 ## Create a folder called FAOSTAT in your working directory
+#dir.create(paste0(wd, "/FAOSTAT/"))
 ## Define a path to the FAOSTAT folder
 data_folder <- paste0(wd,"/FAOSTAT/")
 
 ## Country of interest (specified by the 3-digit ISO code)
 ISO <- "ARG"
 
-# Carbon ton to CO2 conversion
+## Country projection (you can find the proj.4 here: http://epsg.io/ )
+proj <- "+proj=tmerc +lat_0=-90 +lon_0=-60 +k=1 +x_0=5500000 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs "
+
+## Carbon ton to CO2 conversion
 CO2conv <- 3.67
 
 # Set the working directory
 setwd(wd)
 
 # Download the total agricultural emissions form FAOSTAT
-
-#use the link below to download the data once and download it using the
-#download_faostat_bulk function
-#after downloading comment lines 45-46
+# use the link below to download the data once and download it using the
+# download_faostat_bulk function
+# after downloading comment lines 53-54
 
 #all_em <- "http://fenixservices.fao.org/faostat/static/bulkdownloads/Emissions_Agriculture_Agriculture_total_E_All_Data.zip"
 #download_faostat_bulk(all_em, data_folder)
 data <- read_faostat_bulk(paste0(data_folder,"Emissions_Agriculture_Agriculture_total_E_All_Data.zip")) 
 
-#Extract the most recent year
+# Extract the most recent year
 data <- data[, c("area_code","area", "item","y2018","element", "unit")]
 
-#Convert country codes to ISO codes
+# Convert country codes to ISO codes
 data$ISO <- countrycode(data$area_code, origin = 'fao', destination = 'iso3c')
 data <- data[data$ISO == ISO & data$item == "Agriculture total" |data$ISO == ISO &  data$item == "Agricultural Soils",]
 
-#Extract CO2 equivalent
-
+# Extract CO2 equivalent
 data <-data[data$element ==  "Emissions (CO2eq)",]
-
-# Extract total agricultural emissions for country of interest
 data <- na.omit(data)
 
-#Calculate total agricultural emissions in Gigagrams
-totagrem <- sum(data$y2018)
+# Calculate total agricultural emissions in Gigagrams
+totagrem <- round(sum(data$y2018),2)
 
-#Load Output Maps: Relative Sequestration Rates (SSM-BAU)
+# Load Output Maps: Relative Sequestration Rates (SSM-BAU)
 SSM1 <- raster(paste0(GSOCseq_folder, ISO,"_GSOCseq_RSR_SSM1_Map030.tif"))
 SSM2 <- raster(paste0(GSOCseq_folder,ISO, "_GSOCseq_RSR_SSM2_Map030.tif"))
 SSM3 <- raster(paste0(GSOCseq_folder,ISO, "_GSOCseq_RSR_SSM3_Map030.tif"))
 
-#Reproject the maps to Mollweide (this will take some time and output warning messages)
-SSM1 <-projectRaster(SSM1, crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84")
-SSM2 <-projectRaster(SSM2, crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84")
-SSM3 <-projectRaster(SSM3, crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84")
+# Reproject to local projection
+SSM1 <- projectRaster(SSM1, crs = proj)
+SSM2 <- projectRaster(SSM2, crs = proj)
+SSM3 <- projectRaster(SSM3, crs = proj)
 
+# Sum over every pixel to get total sequestration potential 
+totssm1 <- round(((sum(unique(SSM1[!is.na(SSM1)]))*CO2conv*100)/1000),2)
+totssm2 <- round(((sum(unique(SSM2[!is.na(SSM2)]))*CO2conv*100)/1000),2)
+totssm3 <- round(((sum(unique(SSM3[!is.na(SSM3)]))*CO2conv*100)/1000),2)
 
-#Sum over every pixel to get total sequestration potential 
-totssm1 <- (sum(unique(SSM1[!is.na(SSM1)]))*CO2conv*100)/1000
-totssm2 <- (sum(unique(SSM2[!is.na(SSM2)]))*CO2conv*100)/1000
-totssm3 <- (sum(unique(SSM3[!is.na(SSM3)]))*CO2conv*100)/1000
+# Estimate mitigation potential 
+SSM1 <-round(((totssm1/totagrem) *100),2)
+SSM2 <-round(((totssm2/totagrem) *100),2)
+SSM3 <-round(((totssm3/totagrem) *100),2)
 
-
-
-
-#Estimate mitigation potential 
-SSM1 <-(totssm1/totagrem) *100
-SSM2 <-(totssm2/totagrem) *100
-SSM3 <-(totssm3/totagrem) *100
-
-#Crete table 
+# Create table 
 table <- data.frame(Total_Agricultural_Emissions =c("CO2eq gigagrams/year", totagrem, totagrem, totagrem),
                     Scenario = c("/", "SSM1", "SSM2", "SSM3"),
                     C_sequestered =c("CO2eq gigagrams/year",totssm1,totssm2, totssm3),
                     Mitigation_share =c("%",SSM1,SSM2,SSM3))
+
+# Display the table in the console
 table
