4f33e328b597143dd8876453007c12bac24def6e
Isabel Luotto <72745296+Isa-Luotto@users.noreply.github.com>
2021-06-14 09:41:40 +0200

    removed soil emissions

--- extra_scripts/FAOSTAT_GSOCseq.R
+++ extra_scripts/FAOSTAT_GSOCseq.R
@@ -19,22 +19,27 @@ library(sp)
 
 # User defined variables
 
-## Working directory
-wd <- "C:/Users/hp/Documents/FAO/GSOCseq"
-#wd <- "C:/Users/luottoi/Documents/GSOCseq/Argentina"
+## Working directory (wd). It should end with a "/"
+wd <- "C:/Users/hp/Documents/FAO/GSOCseq/"
+#wd <- "C:/Users/luottoi/Documents/GSOCseq/"
 
 
-## GSOCseq output maps directory 
-GSOCseq_folder <- paste0(wd,"/Argentina/")
+## GSOCseq output maps directory. The paste0 function will combine
+## your working directory wd with the folder name that contains
+## your output maps
+GSOCseq_folder <- paste0(wd,"Greece/quality check/")
 
 
 ## Create a folder called FAOSTAT in your working directory
+## you can do it manually or by using the dir.create function 
+## remember to comment the dir.function command after running it once
+
 #dir.create(paste0(wd, "/FAOSTAT/"))
 ## Define a path to the FAOSTAT folder
 data_folder <- paste0(wd,"/FAOSTAT/")
 
 ## Country of interest (specified by the 3-digit ISO code)
-ISO <- "ARG"
+ISO <- "GRC"
 
 
 ## Carbon ton to CO2 conversion
@@ -43,10 +48,10 @@ CO2conv <- 3.67
 # Set the working directory
 setwd(wd)
 
-# Download the total agricultural emissions form FAOSTAT
-# use the link below to download the data once and download it using the
+# Download the total agricultural emissions from FAOSTAT
+# use the link below to download the data once by using the
 # download_faostat_bulk function
-# after downloading comment lines 51-52
+# after downloading comment lines 56-57
 
 #all_em <- "http://fenixservices.fao.org/faostat/static/bulkdownloads/Emissions_Agriculture_Agriculture_total_E_All_Data.zip"
 #download_faostat_bulk(all_em, data_folder)
@@ -55,18 +60,23 @@ data <- read_faostat_bulk(paste0(data_folder,"Emissions_Agriculture_Agriculture_
 # Extract the most recent year
 data <- data[, c("area_code","area", "item","y2018","element", "unit")]
 
-# Convert country codes to ISO codes
+# Convert FAO country codes to ISO codes
 data$ISO <- countrycode(data$area_code, origin = 'fao', destination = 'iso3c')
-data <- data[data$ISO == ISO & data$item == "Agriculture total" |data$ISO == ISO &  data$item == "Agricultural Soils",]
+data <- data[data$ISO == ISO & data$item == "Agriculture total",]
 
 # Extract CO2 equivalent
 data <-data[data$element ==  "Emissions (CO2eq)",]
 data <- na.omit(data)
 
 # Calculate total agricultural emissions in Gigagrams
+# this sums total agricultural emissions in CO2 eq and emissions from soils
 totagrem <- round(sum(data$y2018),2)
 
 # Load Output Maps: Relative Sequestration Rates (SSM-BAU)
+# paste0 will combine the path to the outputs maps contained in
+# GSOCseq_folder and the ISO variable 
+# if you get an error message, make sure that paste0 is creating a correct
+# path
 SSM1 <- raster(paste0(GSOCseq_folder, ISO,"_GSOCseq_RSR_SSM1_Map030.tif"))
 SSM2 <- raster(paste0(GSOCseq_folder,ISO, "_GSOCseq_RSR_SSM2_Map030.tif"))
 SSM3 <- raster(paste0(GSOCseq_folder,ISO, "_GSOCseq_RSR_SSM3_Map030.tif"))
@@ -77,7 +87,7 @@ SSM2 <- SSM2 * area(SSM2)
 SSM3 <- SSM3 * area(SSM3)
 
 
-# Sum over every pixel to get total sequestration potential 
+# Sum over every pixel to get total sequestration potential in gigagrams
 totssm1 <- round(((sum(unique(SSM1[!is.na(SSM1)]))*CO2conv*100)/1000),2)
 totssm2 <- round(((sum(unique(SSM2[!is.na(SSM2)]))*CO2conv*100)/1000),2)
 totssm3 <- round(((sum(unique(SSM3[!is.na(SSM3)]))*CO2conv*100)/1000),2)
